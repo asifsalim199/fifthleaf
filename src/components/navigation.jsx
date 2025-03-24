70e6e9f1e650d22d783cadebc6b54cd860e40fb1
Asif Salim <asifsalim199@gmail.com>
2025-03-24 07:29:20 +0530

    Initial commit

---
+++ src/components/navigation.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+export const Navigation = (props) => {
+  return (
+    
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+  <div className="navbar-header">
+    {/* Logo Image Here */}
+    <img src="/img/logo.jpg" alt="Logo" width="50" height="50" style={{ marginRight: "10px", verticalAlign: "middle" }} />
+
+    {/* Navbar Toggle Button */}
+    <button
+      type="button"
+      className="navbar-toggle collapsed"
+      data-toggle="collapse"
+      data-target="#bs-example-navbar-collapse-1"
+    >
+      <span className="sr-only">Toggle navigation</span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+    </button>
+
+    {/* Brand Name */}
+    <a className="navbar-brand page-scroll" href="#page-top">
+      FIFTH LEAF
+    </a>
+  </div>
+
+
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Recipes
+              </a>
+            </li>
+            {/* <li>
+              <a href="#portfolio" className="page-scroll">
+                Gallery
+              </a>
+            </li> */}
+            {/* <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li> */}
+            {/* <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li> */}
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Look some recipes</h2>
+          <p>
+          Coconut milk is a versatile ingredient that adds a creamy, rich texture to various dishes. Here are some delicious recipes you can try using Fifth Leaf Instant Coconut Powder
+
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
